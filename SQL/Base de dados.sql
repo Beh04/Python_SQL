d3a8362f21343a6dfe81e6a34bafbd357c6f60d4
Bruno Estevam <143735758+Beh04@users.noreply.github.com>
2023-12-21 20:35:54 -0300

    Update Base de dados.sql

--- SQL/Base de dados.sql
+++ SQL/Base de dados.sql
@@ -1,16 +1,16 @@
-create or alter view vw_base_previsao
+create or alter view vw_base_previsao /*Quando formos conectar no Jupyter Notebook no python, iremos puxar pela view*/
 as 
 select top (200000)
 	FS.SalesKey as 'ID da Venda'
 	,format(DT.Datekey,'dd/MM/yyyy') as 'Data da Venda'
 	,DT.CalendarYear as 'Ano'
-	,DT.CalendarMonthLabel as 'Mês'
+	,DT.CalendarMonthLabel as 'MÃªs'
 	,DP.ProductKey as 'ID do Produto'
 	,DP.ProductName as 'Nome do Produto'
 	,DC.ChannelName as 'Nome do Canal'
 	,DPC.ProductCategoryName as 'Nome da Categoria'
 	,DPS.ProductSubcategoryName as 'Nome da Subcategoria'
-	,FS.UnitPrice as 'Preço Unitário'
+	,FS.UnitPrice as 'PreÃ§o UnitÃ¡rio'
 	,FS.SalesQuantity as 'Qtd. Vendida'
 	,FS.UnitPrice * FS.SalesQuantity as 'Faturamento'
 from FactSales as FS inner join	
