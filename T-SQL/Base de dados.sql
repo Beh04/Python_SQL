cf8da48fa0b35e0ea8229e4fc70fc2f5ec7b8fac
Bruno Estevam <brumarketing08@gmail.com>
2023-12-23 10:29:59 -0300

    Retirar a coluna de canais de venda

--- T-SQL/Base de dados.sql
+++ T-SQL/Base de dados.sql
@@ -12,7 +12,6 @@ select top (200000) -- Nossa tabela de vendas possui 3 milhões de linhas, vamos
 	,month(DT.FullDateLabel) as 'Número do Mês'
 	,DP.ProductKey as 'ID do Produto'
 	,DP.ProductName as 'Nome do Produto'
-	,DC.ChannelName as 'Nome do Canal'
 	,DPC.ProductCategoryName as 'Nome da Categoria'
 	,DPS.ProductSubcategoryName as 'Nome da Subcategoria'
 	,FS.UnitPrice as 'Preço Unitário'
@@ -20,8 +19,7 @@ select top (200000) -- Nossa tabela de vendas possui 3 milhões de linhas, vamos
 	,FS.UnitPrice * FS.SalesQuantity as 'Faturamento'
 /*Nesta parte vamos realizar nossos joins com nossas tabelas dimensões para trazermos informações importante para nossa nova base
 que será formada e armezenada na view*/
-from FactSales as FS inner join	
-	DimChannel as DC on FS.channelKey = DC.ChannelKey -- Tabela de Canais de venda
+from FactSales as FS	
 	inner join DimProduct as DP on FS.ProductKey = DP.ProductKey -- Tabela de produtos
 	inner join DimProductSubcategory as DPS on DP.ProductSubcategoryKey = DPS.ProductSubcategoryKey -- Tabela de Subcategoria dos produtos
 	inner join DimProductCategory as DPC on DPS.ProductCategoryKey = DPC.ProductCategoryKey -- Tabela de Categoria dos produtos
